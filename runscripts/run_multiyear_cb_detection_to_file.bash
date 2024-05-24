5a097910036ecb409de72be0cc8d7aafb9b2bae8
romainpilon <romain.pilon@gmail.com>
2024-05-24 11:36:22 +0200

    Add year start and aend as variables in multiyear runscript

--- runscripts/run_multiyear_cb_detection_to_file.bash
+++ runscripts/run_multiyear_cb_detection_to_file.bash
@@ -12,70 +12,87 @@
 # Please adapt #SBATCH --array=0-62 according to the number of year
 # Please check the configuration file parameters of the domain where you want to run the code
 
+# To run: `sbatch run_multiyear_cb_detection_to_file.bash`
 
 module purge
 module load gcc python
 module load geos proj
 
 
+# *-------- DEFINE THESE VARIABLES and SET UP CODE
+# Define start and end year
+YEAR_START=1959
+YEAR_END=2022
+
+source /users/rpilon/codes/unil/cloudbandPy/myenv/bin/activate
+export CLOUDBANDPY_DIR=/users/rpilon/codes/unil/cloudbandPy
+# *--------
+
+
 # Check if cloudbandPy is installed and activate the environment
-if python -c "import cloudbandpy" &> /dev/null; then
-    echo "cloudbandPy is installed, checking for environments."
-    # Check for a Python virtual environment
-    if [[ -n "$VIRTUAL_ENV" ]]; then
-        echo "Using the Python virtual environment."
-        source "${VIRTUAL_ENV}"/bin/activate
-    # Check for a Conda environment
-    elif conda info --envs | grep "$(basename "$CONDA_PREFIX")" &> /dev/null; then
-        echo "Using the Conda environment: $(basename "$CONDA_PREFIX")"
-        source activate "$(basename "$CONDA_PREFIX")"
+activate_env() {
+    if python -c "import cloudbandpy" &> /dev/null; then
+        echo "cloudbandPy is installed, checking for environments."
+        if [[ -n "$VIRTUAL_ENV" ]]; then
+            echo "Using the Python virtual environment."
+            source "${VIRTUAL_ENV}/bin/activate"
+        elif conda info --envs | grep "$(basename "$CONDA_PREFIX")" &> /dev/null; then
+            echo "Using the Conda environment: $(basename "$CONDA_PREFIX")"
+            source activate "$(basename "$CONDA_PREFIX")"
+        else
+            echo "No Python virtual or Conda environment is active. Running the script in the base environment."
+        fi
     else
-        echo "No Python virtual or Conda environment is active. Running the script in the base environment."
+        echo "cloudbandPy is not installed. Please modify this script to load it."
+        exit 1
     fi
-else
-    echo "cloudbandPy is not installed. Please install it before running this script."
-    exit 1
-fi
+}
 
 
 # We need to have access to configuration files
-# Check if the CLOUDBANDPY_DIR environment variable is set
-if [[ -z "${CLOUDBANDPY_DIR}" ]]; then
-    echo "The CLOUDBANDPY_DIR environment variable is not set. Please set it to your cloudbandPy directory path."
-    exit 1
-else
-    echo "Using cloudbandPy directory: ${CLOUDBANDPY_DIR}"
-fi
-export CLOUDBANDPY_DIR=/path/to/your/cloudbandPy
+# Check if cloudbandPy configuration directory is set up
+setup_config_dir() {
+    if [[ -z "${CLOUDBANDPY_DIR}" ]]; then
+        echo "The CLOUDBANDPY_DIR environment variable is not set. Please set it to your cloudbandPy directory path."
+        exit 1
+    else
+        echo "Using cloudbandPy directory: ${CLOUDBANDPY_DIR}"
+    fi
+}
 
 
-# For temporary config files. we create one config file per year to save memory (instead of loading 43 years of reanalysis)
-tmpdir_config=~/tmp
-[[ ! -d "${tmpdir_config}" ]] && mkdir "${tmpdir_config}"
-
-# Select the domain(s)
-declare -a domains=("southPacific" "northPacific" "southIndianOcean" "southAtlantic" "southernhemisphere" "northernhemisphere")
-
-for domain in "${domains[@]}"; do
-    echo "${domain}"
-
-    configname=config_cbworkflow_"${domain}"
-    configpath="${CLOUDBANDPY_DIR}"/config/"${configname}".yml
-
-    years=({1959..2022..1})
-
-    echo "${years["${SLURM_ARRAY_TASK_ID}"]}"
-    outfil="${tmpdir_config}"/"${configname}"_"${years["${SLURM_ARRAY_TASK_ID}"]}".yml
-    old=2016 # check the start/end year
-    new="${years["${SLURM_ARRAY_TASK_ID}"]}"
-    sed "s|${old}|${new}|g" "${configpath}" >"${outfil}"
+# Function to create temporary config files
+create_tmp_config() {
+    local domain=$1
+    local year=$2
+    local configname=config_cbworkflow_"${domain}"
+    local configpath="${CLOUDBANDPY_DIR}/config/${configname}.yml"
+    local outfil="${tmpdir_config}/${configname}_${year}.yml"
+    sed "s|2016|${year}|g" "${configpath}" >"${outfil}"
     sed -i "s|save_cloudbands_netcdf: .*|save_cloudbands_netcdf: True|g" "${outfil}"
+}
 
-    configfilename="${configname}"_"${years["${SLURM_ARRAY_TASK_ID}"]}".yml
-    configpath="${tmpdir_config}"/"${configfilename}"
 
-    echo "${CLOUDBANDPY_DIR}" "${configpath}"
+# Main script execution
+main() {
+    tmpdir_config=~/tmp
+    [[ ! -d "${tmpdir_config}" ]] && mkdir "${tmpdir_config}"
+    declare -a domains=("southPacific") # "northPacific" "southIndianOcean" "southAtlantic" "southernhemisphere" "northernhemisphere")
+    years=($(seq $YEAR_START $YEAR_END))
+    echo $years
+    local year="${years[${SLURM_ARRAY_TASK_ID}]}"
 
-    python "${CLOUDBANDPY_DIR}"/runscripts/run.py "${configpath}"
+    activate_env
+    setup_config_dir
 
-done
+    for domain in "${domains[@]}"; do
+        echo "${domain}"
+        create_tmp_config "${domain}" "${year}"
+        local configfilename=config_cbworkflow_"${domain}_${year}.yml"
+        local configpath="${tmpdir_config}/${configfilename}"
+        echo "${CLOUDBANDPY_DIR}" "${configpath}"
+        python "${CLOUDBANDPY_DIR}/runscripts/run.py" "${configpath}"
+    done
+}
+
+main
